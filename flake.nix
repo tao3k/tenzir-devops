3052d4aeaefbe07daa069e094e0f79e6edafda74
guangtao <gtrunsec@hardenedlinux.org>
2023-07-22 22:42:26 +0000

    feat: devops.users -> enable preset.shell

--- flake.nix
+++ flake.nix
@@ -1,9 +1,13 @@
 {
   description = "The users' interface to the Tenzir platform";
-  outputs = { self, tenzir-devops}@inputs:
-    (tenzir-devops.lib.mkStd ./.) // {
-    devShells = inputs.tenzir-devops.inputs.std.harvest inputs.self [["users" "shells"]];
-  };
+  outputs = {
+    self,
+    tenzir-devops,
+  } @ inputs:
+    (tenzir-devops.lib.mkStd ./.)
+    // {
+      devShells = inputs.tenzir-devops.inputs.std.harvest inputs.self [["users" "shells"]];
+    };
   inputs = {
     tenzir-devops.url = "github:gtrunsec/vast/devops?dir=nix/devops";
   };
