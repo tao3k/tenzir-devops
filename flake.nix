d9608d289bf0a48eba82bcffff8990b055dc8144
guangtao <gtrunsec@hardenedlinux.org>
2023-07-25 05:14:34 +0000

    chore: update lock

--- flake.nix
+++ flake.nix
@@ -3,13 +3,20 @@
   outputs = {
     self,
     tenzir-devops,
+    ...
   } @ inputs:
-    (tenzir-devops.lib.mkStd ./.)
+    (
+      tenzir-devops.lib.mkStd
+      {
+        inherit inputs;
+        cellsFrom = ./cells;
+      }
+    )
     // {
       devShells = inputs.tenzir-devops.inputs.std.harvest inputs.self [["users" "shells"]];
     };
   inputs = {
     tenzir-devops.url = "github:gtrunsec/vast/devops?dir=nix/devops";
-    # tenzir-devops.url = "/home/guangtao/ghq/github.com/tenzir/vast/nix/devops";
+    nixpkgs.follows = "tenzir-devops/nixpkgs";
   };
 }
