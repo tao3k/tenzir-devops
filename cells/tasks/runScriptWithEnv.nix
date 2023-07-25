81accbae3f486f8a3c1084c4f4bb5e3f1f3801d9
guangtao <gtrunsec@hardenedlinux.org>
2023-07-25 07:08:53 +0000

    refactor: update mkDevOCI

---
+++ cells/tasks/runScriptWithEnv.nix
@@ -0,0 +1,11 @@
+makeScript {
+  name = "runScriptWithEnv";
+  env = {
+    a = "1";
+    b = "2";
+    VAR_NAME = "test";
+  };
+  searchPaths.bin = [nixpkgs.hello];
+  searchPaths.source = [];
+  entrypoint = "hello --help echo $a";
+}
